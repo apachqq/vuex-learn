8de83849ae08664145c3814af3f3fdd753c4dc7d
apachqq <work.tsyganov@mail.ru>
2022-04-27 19:21:58 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <the-navbar></the-navbar>
+    <div class="container with-nav">
+        <div class="card">
+            <h1>Про Vuex</h1>
+            <h2>Счетчик 0</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TheNavbar from '@/TheNavbar'
+
+    export default {
+        components: {TheNavbar}
+    }
+</script>
+
+<style>
+</style>
---
+++ src/TheNavbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="navbar">
+        Vuex
+    </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>

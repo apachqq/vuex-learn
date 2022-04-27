7c38f1722131472ec7c74be9868568f223ed00aa
apachqq <work.tsyganov@mail.ru>
2022-04-27 19:41:55 +0300

    start-work-with-Store

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,23 @@
     <div class="container with-nav">
         <div class="card">
             <h1>Про Vuex</h1>
-            <h2>Счетчик 0</h2>
+            <h2>Счетчик {{ $store.state.counter }}</h2>
+            <button class="btn" @click="increment">Добавить</button>
         </div>
     </div>
 </template>
 
 <script>
-    import TheNavbar from '@/TheNavbar'
+    import TheNavbar from '@/components/TheNavbar'
 
     export default {
-        components: {TheNavbar}
+        components: {TheNavbar},
+
+        methods: {
+            increment () {
+                this.$store.state.counter++
+            }
+        }
     }
 </script>
 
---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    state() {
+        return {
+            counter: 0
+        }
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    modules: {
+
+    }
+})

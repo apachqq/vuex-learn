fe009141cbde23bc06e4fd97752624939e3ef578
apachqq <work.tsyganov@mail.ru>
2022-04-28 13:47:22 +0300

    mappers

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,31 @@
     <div class="container with-nav">
         <div class="card">
             <h1>Про Vuex</h1>
-            <h2>Счетчик {{ $store.getters.counter }} ({{ $store.getters.doubleCounter }})</h2>
+            <h2>Счетчик {{ counter }} ({{ doubleCounter }})</h2>
             <button class="btn primary" @click="add">Добавить</button>
-            <button class="btn danger" @click="incrementAsync">Добавить 10</button>
+            <button class="btn danger" @click="incrementAsync({value: 10, delay: 200})">Добавить 10</button>
         </div>
     </div>
 </template>
 
 <script>
+    import { mapGetters, mapMutations, mapActions } from 'vuex'
     import TheNavbar from '@/components/TheNavbar'
 
     export default {
         components: {TheNavbar},
-
+        computed: {
+            ...mapGetters(['counter', 'doubleCounter'])
+        },
         methods: {
-            add () {
-                this.$store.commit('increment')
-            },
-            incrementAsync () {
-                this.$store.dispatch('incrementAsync', {
-                    value: 10,
-                    delay: 200
-                })
-            }
+            ...mapMutations({add: 'increment'}),
+            ...mapActions(['incrementAsync'])
+            // incrementAsync() {
+            //     this.$store.dispatch('incrementAsync', {
+            //         value: 10,
+            //         delay: 200
+            //     })
+            // }
         }
     }
 </script>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -6,25 +6,37 @@
 </template>
 
 <script>
+    import { mapGetters, mapMutations } from 'vuex'
+
     export default {
         methods: {
+            ...mapMutations({
+                addFive: 'add'
+            }),
             add() {
+                this.addFive({
+                    value: 5
+                })
+                // this.add({
+                //     value: 5
+                // })
                 // this.$store.commit('add', {
                 //     value: 5
                 // })
                 //
                 // Идентичные записи
                 //
-                this.$store.commit({
-                    type: 'add',
-                    value: 5
-                })
+                // this.$store.commit({
+                //     type: 'add',
+                //     value: 5
+                // })
             }
         },
         computed: {
-            counter() {
-                return this.$store.getters.counter
-            }
+            // counter() {
+            //     return this.$store.getters.counter
+            // },
+            ...mapGetters(['counter'])
         }
     }
 </script>
